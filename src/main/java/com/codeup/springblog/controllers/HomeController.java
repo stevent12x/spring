e727064237c0b3320e8cd39560d6ab31c0184fe4
Steven Turner <steven.turner12x@gmail.com>
2019-09-06 13:56:21 -0500

    setting up roll-dice framework

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -1,5 +1,4 @@
 package com.codeup.springblog.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,5 +9,11 @@ public class HomeController {
     @ResponseBody
     public String hello() {
         return "<h2>This is the landing page!</h2>";
+    }
+
+//    Dice Roll
+    @GetMapping("/roll-dice")
+    public String dice() {
+        return "Pick a number. Roll the dice.";
     }
 }
