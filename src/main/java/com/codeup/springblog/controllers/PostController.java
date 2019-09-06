0376ec8f83adc8eb097298a0de56667ffdc36e2f
Steven Turner <steven.turner12x@gmail.com>
2019-09-06 10:28:58 -0500

    PostController walkthrough

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -8,18 +8,27 @@ class PostController {
     @GetMapping(path = "/posts")
     @ResponseBody
     public String welcome(){
-        return "<h3>Here are all the posts!</h3>";
+        return "<h3>Here are all the posts!</h3>"+
+                "<ul>"+
+                "<li>1</li>"+
+                "<li>2</li>"+
+                "<li>3</li>"+
+                "<li>4</li>"+
+                "</ul>";
     }
+
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public String individualPost(@PathVariable int id){
+    public String individualPost(@PathVariable long id){
         return "<h2>Here is Post #"+id+".</h2>";
     }
+
     @GetMapping(path = "/posts/create")
     @ResponseBody
     public String create(){
         return "<h2>Write a new post here!</h2>";
     }
+
     @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
     @ResponseBody
     public String post(){
