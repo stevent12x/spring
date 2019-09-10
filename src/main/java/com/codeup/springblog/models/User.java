7bac8e40822b5ee7adf73fdc584d5460c4f4514f
Steven Turner <steven.turner12x@gmail.com>
2019-09-10 10:29:04 -0500

    cleaning up

--- src/main/java/com/codeup/springblog/models/User.java
+++ src/main/java/com/codeup/springblog/models/User.java
@@ -9,8 +9,7 @@ import javax.persistence.*;
 @Table(name = "users")
 public class User {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     private long id;
 
     @Column(nullable = false)
@@ -31,35 +30,19 @@ public class User {
         this.password = password;
     }
 
-    public long getId() {
-        return id;
-    }
+    public long getId() { return id; }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+    public void setId(long id) { this.id = id; }
 
-    public String getUsername() {
-        return username;
-    }
+    public String getUsername() { return username; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getEmail() {
-        return email;
-    }
+    public String getEmail() { return email; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public void setEmail(String email) { this.email = email; }
 
-    public String getPassword() {
-        return password;
-    }
+    public String getPassword() { return password; }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public void setPassword(String password) { this.password = password; }
 }
