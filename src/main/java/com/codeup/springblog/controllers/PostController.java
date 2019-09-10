92d3e8721f1dbc87bed633931657c7f46eac1ab0
Steven Turner <steven.turner12x@gmail.com>
2019-09-10 11:31:39 -0500

    working on test branch now

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -74,29 +74,4 @@ class PostController {
         Post newPost = postDao.save(postToBeCreated);
         return "redirect:/show/" + newPost.getId();
     }
-
-
-
-
-
-
-
-
-//    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-//    @ResponseBody
-//    public String individualPost(@PathVariable long id){
-//        return "<h2>Here is Post #"+id+".</h2>";
-//    }
-//
-//    @GetMapping(path = "/posts/create")
-//    @ResponseBody
-//    public String create(){
-//        return "<h2>Write a new post here!</h2>";
-//    }
-//
-//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-//    @ResponseBody
-//    public String post(){
-//        return "<h2>Your post has been posted!</h2>";
-//    }
 }
