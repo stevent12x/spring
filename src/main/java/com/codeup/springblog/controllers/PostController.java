f7081a9463b6832a07b11ed6e2dd7bed5213ad2d
Steven Turner <steven.turner12x@gmail.com>
2019-09-11 10:27:08 -0500

    reformatting edit/delete function

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -6,10 +6,7 @@ import com.codeup.springblog.repos.PostRepository;
 import com.codeup.springblog.repos.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 class PostController {
@@ -44,27 +41,29 @@ class PostController {
     }
 
     @GetMapping("/posts/create")
-    public String showCreateForm() {
+    public String showCreateForm(Model model) {
+        model.addAttribute("post", new Post());
         return "/create";
     }
 
     @PostMapping("/posts/create")
     public String createPost(
-            @RequestParam(name = "title") String titleParam,
-            @RequestParam(name = "content") String contentParam,
-            @RequestParam(name = "authorFirstName") String authorFirstNameParam,
-            @RequestParam(name = "authorLastName") String authorLastNameParam
+//            @RequestParam(name = "title") String titleParam,
+//            @RequestParam(name = "content") String contentParam,
+//            @RequestParam(name = "authorFirstName") String authorFirstNameParam,
+//            @RequestParam(name = "authorLastName") String authorLastNameParam
+            @ModelAttribute Post post
     ) {
         User userDB = userDao.findOne(1L);
-        Post postToBeCreated = new Post();
-
-        postToBeCreated.setTitle(titleParam);
-        postToBeCreated.setContent(contentParam);
-        postToBeCreated.setAuthorFirstName(authorFirstNameParam);
-        postToBeCreated.setAuthorLastName(authorLastNameParam);
-        postToBeCreated.setUser(userDB);
-
-        Post newPost = postDao.save(postToBeCreated);
-        return "redirect:/show/" + newPost.getId();
+//        Post postToBeCreated = new Post();
+//
+//        postToBeCreated.setTitle(titleParam);
+//        postToBeCreated.setContent(contentParam);
+//        postToBeCreated.setAuthorFirstName(authorFirstNameParam);
+//        postToBeCreated.setAuthorLastName(authorLastNameParam);
+        post.setUser(userDB);
+        postDao.save(post);
+//        Post newPost = postDao.save(postToBeCreated);
+        return "redirect:/show/" + post.getId();
     }
 }
